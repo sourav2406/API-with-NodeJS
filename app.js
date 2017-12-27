9135320fbd54622cf222287f84a5723913d2647c
Sourav Mandal <sourav2406@gmail.com>
2017-12-28 00:12:04 +0530

    Setting the mongo db with mongoose

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 var express = require('express');
+var mongoose = require('mongoose');
+
+var db = mongoose.connect('mongodb://booksAPI');
+
+var Book = require('./models/bookModel');
 
 var app = express();
 
 var port = process.env.PORT || 3000;
 
+var bookrouter = express.Router();
+
+bookrouter.route('/Books')
+    .get(function(req,res){
+        Book.find(function(err,books){
+            if(err){
+                res.status(500).send(err);
+            }
+            else{
+                res.json(books);
+            }
+        });
+    });
+
+app.use('/api',bookrouter);
+
 app.get('/',function(req,res){
-    res.send("Welcome to Sourav's API site huha..!!");
+    res.send("Welcome to Sourav's API site ..!!");
 });
 
 app.listen(port,function(){
